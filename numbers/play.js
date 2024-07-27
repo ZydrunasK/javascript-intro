0c0d5046e18a611910abb83acfcb763fb4dc4b8b
Zydrunask <kniukstaz@gmail.com>
2024-07-27 15:02:11 +0300

    playing around

---
+++ numbers/play.js
@@ -0,0 +1,36 @@
+let a = 1;
+let b = 2;
+
+a = 3;
+
+const c = a + b;
+
+let ca = c * a;
+let cb = c * b;
+
+
+
+
+console.log('a =', a);
+console.log('b =', b);
+console.log('ca =', 'c * a =', ca);
+console.log('cb =', 'c * b =', cb);
+console.log('ca < cb: ', Boolean (ca < cb), '(', ca, '<', cb, ')');
+console.log('ca > cb: ', Boolean (ca > cb), '(', ca, '>', cb, ')');
+console.log('ca = cb: ', Boolean (ca == cb), '(', ca, "=", cb, ')');
+
+console.clear();
+// simple math
+
+let q = 2;
+let w = 5;
+let e = 8;
+
+const r = (q * w)/ e;
+
+console.log('r = (',q,'*',w,') /',e);
+console.log(r);
+
+
+
+
