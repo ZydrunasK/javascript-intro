986380804005e82cddc39817be0ef6c133cf75f2
Zydrunask <kniukstaz@gmail.com>
2024-08-04 16:56:44 +0300

    improved cycle

--- numbers/play.js
+++ numbers/play.js
@@ -1,120 +1,58 @@
-let a = 1;
-let b = 2;
-
-a = 3;
-
-const c = a + b;
-
-let ca = c * a;
-let cb = c * b;
+let a = 0;
+for (let i = 0; i <= 0; i++) {
+    a += i;
+}
+console.log(`a. sum is ${a}`);   
+console.log('---------');
 
 
+let b = 0;
+for (let i = 0; i <= 4; i++) {
+    b += i;
+}
+console.log(`b. sum is ${b}`);   
+console.log('---------');
 
 
-console.log('a =', a);
-console.log('b =', b);
-console.log('ca =', 'c * a =', ca);
-console.log('cb =', 'c * b =', cb);
-console.log('ca < cb: ', Boolean (ca < cb), '(', ca, '<', cb, ')');
-console.log('ca > cb: ', Boolean (ca > cb), '(', ca, '>', cb, ')');
-console.log('ca = cb: ', Boolean (ca == cb), '(', ca, "=", cb, ')');
-
-console.clear();
-// simple math
-
-let q = 2;
-let w = 5;
-let e = 8;
-
-const r = (q * w)/ e;
-
-console.log('r = (',q,'*',w,') /',e);
-console.log(r);
-
-console.log('----------------------------');
-
-console.log(2 || 4);
-console.log(4 || 2);
-console.log(5 || 7);
-console.log(8 && 9);
-console.log(4 && 8);
-console.log(2 && 1);
-console.log(2 && 1);
-console.log(1 && 2 || 3);
-console.log(1 || 2 && 3);
-
-console.clear();
-
-const stuff = 2 + 5 * (4*8) - 42;
-const stuff2 = 46+59*748-123;
-const stuff3 = (48*(48*5))/(45+95);
+let c = 0;
+for (let i = 0; i <= 100; i++) {
+    c += i;
+}
+console.log(`c. sum is ${c}`);   
+console.log('---------');
 
 
-console.log(stuff);
-console.log(stuff2);
-console.log(stuff3);
-
-console.clear();
-
-let index = 0;
-
-const skaiciai = [5, 4, 6, 8, 7, 10, 2, 1, 3];
-
-let skaiciuSum = skaiciai[index++];
-skaiciuSum += skaiciai[index++];
-skaiciuSum += skaiciai[index++];
-skaiciuSum += skaiciai[index++];
-skaiciuSum += skaiciai[index++];
-skaiciuSum += skaiciai[index++];
-skaiciuSum += skaiciai[index++];
-skaiciuSum += skaiciai[index++];
-skaiciuSum += skaiciai[index++];
-
-console.log('kiekis - ', skaiciai.length);
-console.log('current index', index++);
-console.log(skaiciuSum);
-console.clear();
-
-const pc = {
-    monitor: {
-        width: 1920,
-        height: 1080,
-    },
-    ram: 16,
-    hd: 100,
-    keyboard: 'en',
-    mouse: true,
-    power: true,
-    'cpu': 'On Fire!',
-    123: {
-        one: 1,
-        two: 2,
-        three: 3,
-    },
-};
-
-const param = 'monitor';
-const resW = 'width';
-const resH = 'height';
-
-console.log(pc[param][resW]);
-console.log(pc['cpu']);
-console.log(pc['123']['two']);
-
-console.clear();
-
-const house = {
-    width: 16,
-    height: 3,
-    depth: 5,
-};
-
-const houseKeys = Object.keys(house);
-console.log(houseKeys);
-console.log(Object.keys(house));
+let d = 0;
+for (let i = 574; i <= 815; i++) {
+    d += i;
+}
+console.log(`d. sum is ${d}`);   
+console.log('---------');
 
 
+let e = 0;
+for (let i = -50; i <= 50; i++) {
+    e += i;
+}
+console.log(`e. sum is ${e}`);   
+console.log('---------');
 
 
+let f = 0;
+for (let i = -70; i <= 30; i++) {
+    f += i;
+}
+console.log(`f. sum is ${f}`);   
+console.log('---------');
+
+console.log('Numba two cycle');
+
+const word = 'labas';
+let revWord = '';
+for (let i = word.length - 1; i >= 0; i--) {
+    revWord += word[i];
+
+}
+console.log(revWord);
 
 
