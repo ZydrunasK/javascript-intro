418c54ba1d8897b60f3e2ec0cc3d1ef0228df851
Zydrunask <kniukstaz@gmail.com>
2024-08-20 11:03:11 +0300

    random

--- methods/math.js
+++ methods/math.js
@@ -7,3 +7,63 @@ console.log(Math.abs(5));
 console.log(Math.abs(-5));
 
 console.log(Math);
+
+console.clear();
+// rounds to nearest bigger number
+console.log(Math.ceil(-4));
+console.log(Math.ceil(-3.9999));
+console.log(Math.ceil(-3.5));
+console.log(Math.ceil(-3.001));
+console.log(Math.ceil(-3));
+console.log(Math.ceil(0));
+console.log(Math.ceil(7));
+console.log(Math.ceil(7.001));
+console.log(Math.ceil(7.999));
+console.log(Math.ceil(8));
+
+console.clear();
+// rounds to nearest smaller number
+console.log(Math.floor(-4));
+console.log(Math.floor(-3.9999));
+console.log(Math.floor(-3.5));
+console.log(Math.floor(-3.001));
+console.log(Math.floor(-3));
+console.log(Math.floor(0));
+console.log(Math.floor(7));
+console.log(Math.floor(7.001));
+console.log(Math.floor(7.999));
+console.log(Math.floor(8));
+
+console.clear();
+// rounds normally
+console.log(Math.round(-4));
+console.log(Math.round(-3.9999));
+console.log(Math.round(-3.5));
+console.log(Math.round(-3.001));
+console.log(Math.round(-3));
+console.log(Math.round(0));
+console.log(Math.round(7));
+console.log(Math.round(7.001));
+console.log(Math.round(7.999));
+console.log(Math.round(8));
+
+console.clear();
+// removes the decimal part
+console.log(Math.trunc(-4));
+console.log(Math.trunc(-3.9999));
+console.log(Math.trunc(-3.5));
+console.log(Math.trunc(-3.001));
+console.log(Math.trunc(-3));
+console.log(Math.trunc(0));
+console.log(Math.trunc(7));
+console.log(Math.trunc(7.001));
+console.log(Math.trunc(7.999));
+console.log(Math.trunc(8));
+
+
+// Math.max
+console.clear();
+console.log(Math.max(1, 2));
+console.log(Math.max(2, 5, -4));
+
+
